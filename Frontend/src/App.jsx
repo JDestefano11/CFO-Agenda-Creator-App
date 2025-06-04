af15aae69759425f5efe898f98e7fa024337717d
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2025-06-04 18:23:59 -0400

    - Created a login and signup page for users. - Broke the signup page to multiple components for maintainability and easier to modify later. - Added helper functions for localstorage and for password strength in utils file for code maintainability. - Connected React Router in between the new pages. - Created a basic home page since this will be tested by users and the app is part of multiple projects.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
     <>
-      <Navbar />
       <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Home />
+            </>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
